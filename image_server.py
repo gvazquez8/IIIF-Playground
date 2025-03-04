f94f3e8caa2d5b8d4470551acee95866a7d9ddce
Gabriel Vazquez Jr <gabe.vazquez1999@gmail.com>
2025-03-04 02:43:32 -0800

    Create CIIIFImageRequest and parse uri into its IIIF canonical uri form

--- image_server.py
+++ image_server.py
@@ -3,8 +3,10 @@ from PIL import Image
 from base64 import b64encode
 from region_handler import transform_region
 from size_handler import transform_size
+from CIIIFImageRequest import CIIIImageRequest
 import os
 import io
+import json
 
 CAT_IMAGES_FOLDER = os.path.join('static', 'cats')
 
@@ -25,21 +27,26 @@ def main_entry():
 def error(id):
     return "error", 404
 
-@app.route("/<id>/<region>/<size>", methods=['GET'])
-def render_image(id, region, size):
+@app.route("/<id>/<region>/<size>/<rotation>/<quality>.<format>", methods=['GET'])
+def render_image(id, region, size, rotation, quality, format):
 
-    # Full Image Extraction
-    full_filepath = os.path.join(app.config['CATS_FOLDER'], f'{id}.jpg')
+    # Create ImageRequest obj with info.json
+    # ImageRequest will convert URI into its standardized form.
+    image_info_file_path = os.path.join(app.config['CATS_FOLDER'], f'{id}', 'info.json')
     
+    with open(image_info_file_path, 'r') as image_info_file:
+        image_info = json.load(image_info_file)
+    
+    image_req = CIIIImageRequest(id, image_info, region, size, rotation, quality, format)
+
     img_io = io.BytesIO()
     
     # Region Transform
-    result_image = transform_region(full_filepath, region)
+    # result_image = transform_region(full_filepath, region)
     
     # Size Transform
-    root_img = transform_size(result_image, size)
-
-    root_img.save(img_io, format='png')
-    img_io.seek(0)
-    dataurl = 'data:image/png;base64,' + b64encode(img_io.getvalue()).decode('ascii')
-    return render_template("image.html", image_data = dataurl)
+    # root_img = transform_size(result_image, size)
+    # root_img.save(img_io, format='png')
+    # img_io.seek(0)
+    # dataurl = 'data:image/png;base64,' + b64encode(img_io.getvalue()).decode('ascii')
+    # return render_template("image.html", image_data = dataurl)
--- size_handler.py
+++ size_handler.py
@@ -1,9 +1,4 @@
 from PIL import Image
 
 def transform_size(image: Image.Image, size: str) -> Image.Image:
-
-    if size == "max":
-        return image
     
-
-    return
