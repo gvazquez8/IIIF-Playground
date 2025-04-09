967563f2d46297c7fc2abe96d6d6f978dd8b9317
gvazquez8 <gabvazquez@ucdavis.edu>
2025-04-09 02:00:30 -0700

    Add Title and setup server connection menu bar

--- client/winui/MainWindow.xaml.cs
+++ client/winui/MainWindow.xaml.cs
@@ -1,19 +1,4 @@
-using PlaygroundClient.ViewModels;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Imaging;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 namespace PlaygroundClient;
 
@@ -22,5 +7,14 @@ public sealed partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnConnectMenuFlyoutItemClick(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void OnDisonnectMenuFlyoutItemClick(object sender, RoutedEventArgs e)
+    {
+
     }
 }
