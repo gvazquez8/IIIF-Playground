83ea6ab333c529c5a6816f7509d6e4ccda227210
Gabriel Vazquez Jr <gabe.vazquez1999@gmail.com>
2025-04-30 14:15:04 -0700

    Disable Primary Button Enablement if port numberbox is empty

---
+++ client/winui/Utils/BindingUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaygroundClient.Utils;
+public class BindingUtils
+{
+    public static bool IsEmpty(string? str) => string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
+    public static bool IsNotEmpty(string? str) => !IsEmpty(str);
+}
